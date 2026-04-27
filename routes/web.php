a648a369983443f64266acba142e29985f821723
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-04-27 19:23:03 +0330

    change welcome tsx to blade

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
