32db46eb67d283e66dbf67dd80209e12474d403c
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-05 14:59:29 +0330

    add store and update requests for course

---
+++ app/Http/Requests/StoreCourseRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Course;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class StoreCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('create', Course::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'slug' => 'required|string|max:255|unique:courses,slug',
+            'description' => 'nullable|string|max:4096',
+            'thumbnail' => 'nullable|string|max:255',
+            'intro_video_url' => 'nullable|string|max:255',
+            'status' => 'nullable|in:draft,published',
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdateCourseRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class UpdateCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Gate::allows('update', request()->course);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'slug' => 'required|string|max:255|unique:courses,slug,'. request()->course->id,
+            'description' => 'nullable|string|max:4096',
+            'thumbnail' => 'nullable|string|max:255',
+            'intro_video_url' => 'nullable|string|max:255',
+            'status' => 'nullable|in:draft,published',
+        ];
+    }
+}
