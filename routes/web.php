4e1a5796c5261bd9ab13afba228fbe3b99ddde98
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-05 15:00:57 +0330

    add course crud routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::view('/', 'welcome')->name('home');
 
@@ -10,6 +10,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::resource('course', CourseController::class);
 });
 
 require __DIR__.'/settings.php';
