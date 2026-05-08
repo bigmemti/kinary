10927a1f8c61814c1ad7dee84c9918ddaf6b5239
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-08 17:19:50 +0330

    add plans in course show page

--- resources/js/pages/course/show.tsx
+++ resources/js/pages/course/show.tsx
@@ -1,9 +1,17 @@
 import { DashboardContainer } from "@/components/dashboard";
+import InputError from "@/components/input-error";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import AppLayout from "@/layouts/app-layout";
 import { dashboard } from "@/routes";
 import { index } from "@/routes/course";
+import { store } from "@/routes/course/plan";
 import { BreadcrumbItem, Course } from "@/types";
-import { Head } from "@inertiajs/react";
+import { Form, Head } from "@inertiajs/react";
+import { Plus } from "lucide-react";
+import { useState } from "react";
 
 export default function Show({ course }: { course: Course}) {
     const breadcrumbs: BreadcrumbItem[] = [
@@ -24,6 +32,7 @@ export default function Show({ course }: { course: Course}) {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title={course.title} />
+            <PlanCreateDialog course={course} />
             <DashboardContainer>
                 <div>
                     <div>{course.id}</div>
@@ -37,7 +46,99 @@ export default function Show({ course }: { course: Course}) {
                     <div>{course.updated_at}</div>
                     <div>{course.deleted_at}</div>
                 </div>
+                {course.plans?.map((plan) => (
+                    <div key={plan.id}>
+                        <div>{plan.name}</div>
+                        <div>{plan.price}</div>
+                    </div>
+                ))}
             </DashboardContainer>
         </AppLayout>
+    );
+}
+
+
+function PlanCreateDialog({ course }: { course: Course}){
+    const [open, setOpen] = useState(false);
+
+    return(
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button className='self-end' onClick={() => setOpen(true)}>
+                    Create new plan <Plus />
+                </Button>
+            </DialogTrigger>
+
+            <DialogContent>
+                <DialogTitle>
+                    Create a new Course.
+                </DialogTitle>
+
+                <Form
+                    {...store.form(course)}
+                    options={{
+                        preserveScroll: true,
+                    }}
+                    resetOnSuccess
+                    onSuccess={() => setOpen(false)}
+                    className="space-y-6"
+                >
+                    {({ processing, errors }) => (
+                        <>
+                            <div className="grid gap-2">
+                                <Label
+                                    htmlFor="name"
+                                    className="sr-only"
+                                >
+                                    Name
+                                </Label>
+
+                                <Input
+                                    id="name"
+                                    type="text"
+                                    name="name"
+                                    placeholder="Name"
+                                />
+
+                                <InputError message={errors.name} />
+                            </div>
+
+                            <div className="grid gap-2">
+                                <Label
+                                    htmlFor="price"
+                                    className="sr-only"
+                                >
+                                    Price
+                                </Label>
+
+                                <Input
+                                    id="price"
+                                    type="number"
+                                    step={1000}
+                                    name="price"
+                                    placeholder="Price"
+                                />
+
+                                <InputError message={errors.price} />
+                            </div>
+
+                            <DialogFooter className="gap-2">
+                                <Button
+                                    disabled={processing}
+                                    asChild
+                                >
+                                    <button
+                                        type="submit"
+                                        data-test="submit-course-create-button"
+                                    >
+                                        Store
+                                    </button>
+                                </Button>
+                            </DialogFooter>
+                        </>
+                    )}
+                </Form>
+            </DialogContent>
+        </Dialog>
     );
 }
