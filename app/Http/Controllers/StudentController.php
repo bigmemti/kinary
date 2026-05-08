aeba77b955a9f77240fbdd01450d938bec595437
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-08 18:14:06 +0330

    add student controller

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+
+class StudentController extends Controller
+{
+    public function course(){
+        return Inertia::render('study',[
+            'courses' => Course::whereRelation('plans.users', 'id', Auth::user()->id)->get(),
+        ]);
+    } 
+}
