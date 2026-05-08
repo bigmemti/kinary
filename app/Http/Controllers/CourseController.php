dc714190e5ed7ee80c6ca201a93deb8757a68b10
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-08 15:17:36 +0330

    add crud course controller

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCourseRequest;
+use App\Http\Requests\UpdateCourseRequest;
+use App\Models\Course;
+use Inertia\Inertia;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Inertia::render('course/index', [
+            'courses' => Course::all(),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCourseRequest $request)
+    {
+        Course::create($request->validated());
+
+        return;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Course $course)
+    {
+        return Inertia::render('course/show', [
+            'course' => $course,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Course $course)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCourseRequest $request, Course $course)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Course $course)
+    {
+        $course->delete();
+    }
+}
