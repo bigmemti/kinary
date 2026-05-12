ed35e6ed138b294bff51133f3599a1eca1fdd9f0
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-12 16:05:53 +0330

    add layout to blade part of resources

--- resources/views/course-show.blade.php
+++ resources/views/course-show.blade.php
@@ -1,89 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        {{-- Inline script to detect system dark mode preference and apply it immediately --}}
-        <script>
-            (function() {
-                const appearance = '{{ $appearance ?? "system" }}';
-
-                if (appearance === 'system') {
-                    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-                    if (prefersDark) {
-                        document.documentElement.classList.add('dark');
-                    }
-                }
-            })();
-        </script>
-
-        {{-- Inline style to set the HTML background color based on our theme in app.css --}}
-        <style>
-            html {
-                background-color: oklch(1 0 0);
-            }
-
-            html.dark {
-                background-color: oklch(0.145 0 0);
-            }
-        </style>
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        {{-- <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" /> --}}
-
-        <!-- Styles / Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/alpine.js'])
-    </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
-            @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
-                    @auth
-                        <a
-                            href="{{ url('/dashboard') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
-                        >
-                            Dashboard
-                        </a>
-                    @else
-                        <a
-                            href="{{ route('login') }}"
-                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
-                        >
-                            Log in
-                        </a>
-
-                        @if (Route::has('register'))
-                            <a
-                                href="{{ route('register') }}"
-                                class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                                Register
-                            </a>
-                        @endif
-                    @endauth
-                </nav>
-            @endif
-        </header>
-        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <main class="flex w-full flex-col lg:max-w-4xl gap-4">
-                <div class="dark:text-white">{{ $course->title }}:</div>
-                @forelse ($course->plans as $plan)
-                    <div class="dark:text-white">
-                        <div>{{ $plan->name }}</div>
-                        <div>{{ $plan->price }}</div>
-                        <div>
-                            <a href="{{ route('plan.buy', ['plan' => $plan]) }}">buy</a>
-                        </div>
-                    </div>
-                @empty
-                    
-                @endforelse
-            </main>
+<x-layouts.web>
+    <div class="dark:text-white">{{ $course->title }}:</div>
+    @forelse ($course->plans as $plan)
+        <div class="dark:text-white">
+            <div>{{ $plan->name }}</div>
+            <div>{{ $plan->price }}</div>
+            <div>
+                <a href="{{ route('plan.buy', ['plan' => $plan]) }}">buy</a>
+            </div>
         </div>
-    </body>
-</html>
+    @empty
+        
+    @endforelse
+</x-layouts.web>
