0b73b60c2e0e5e3b21fce41542e2d4ecf85fc7e0
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-13 14:30:39 +0330

    add order_plan table

---
+++ database/migrations/2026_05_13_104418_create_order_plan_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Order;
+use App\Models\Plan;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_plan', function (Blueprint $table) {
+            $table->foreignIdFor(Order::class)->constrained();
+            $table->foreignIdFor(Plan::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_plan');
+    }
+};
