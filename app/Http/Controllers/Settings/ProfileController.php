dbf196a3ec053934138d4829ae094e99815dfcbc
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-13 17:09:34 +0330

    fix redirect after delete account bug

--- app/Http/Controllers/Settings/ProfileController.php
+++ app/Http/Controllers/Settings/ProfileController.php
@@ -43,7 +43,7 @@ class ProfileController extends Controller
     /**
      * Delete the user's account.
      */
-    public function destroy(Request $request): RedirectResponse
+    public function destroy(Request $request): Void
     {
         $request->validate([
             'password' => ['required', 'current_password'],
@@ -58,6 +58,6 @@ class ProfileController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return;
     }
 }
