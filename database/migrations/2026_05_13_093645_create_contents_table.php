0d07166af607d5d0217a1a971acd3ab5825323fb
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-13 14:25:58 +0330

    add contents table

---
+++ database/migrations/2026_05_13_093645_create_contents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Lesson;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Lesson::class)->constrained();
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
