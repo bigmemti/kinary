6fd25e8b865ae59afe3d4b1323b6b62bdfcadc12
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-13 14:30:22 +0330

    add orders table

---
+++ database/migrations/2026_05_13_104409_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Wallet;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Wallet::class)->constrained();
+            $table->enum('status', ['pending', 'success', 'failed'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
