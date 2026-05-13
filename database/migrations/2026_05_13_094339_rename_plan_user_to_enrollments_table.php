ea1584655a3e49e301b33f6332e46b09c1706ef2
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-13 14:30:08 +0330

    change plan_user table to enrollments and change foreign key to student

---
+++ database/migrations/2026_05_13_094339_rename_plan_user_to_enrollments_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Models\Student;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('plan_user', function (Blueprint $table) {
+            $table->dropForeign('plan_user_user_id_foreign'); 
+            $table->dropForeign('plan_user_plan_id_foreign'); 
+            $table->dropIndex('plan_user_user_id_foreign');
+            $table->dropUnique('PUI');
+
+            $table->dropForeignIdFor(User::class);
+        });
+
+        Schema::rename('plan_user', 'enrollments');
+
+        Schema::table('enrollments', function (Blueprint $table) {
+            $table->foreignIdFor(Student::class)->after('plan_id')->constrained();
+
+            $table->foreign('plan_id')->references('id')->on('plans');
+
+            $table->unique(['plan_id', 'student_id'], 'PSI');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('enrollments', function (Blueprint $table) {
+            $table->dropForeign('enrollments_student_id_foreign'); 
+            $table->dropForeign('enrollments_plan_id_foreign'); 
+            $table->dropIndex('enrollments_student_id_foreign');
+            $table->dropUnique('PUI');
+
+            $table->dropForeignIdFor(Student::class);
+
+        });
+
+        Schema::rename('enrollments', 'plan_user');
+
+        Schema::table('plan_user', function (Blueprint $table) {
+            $table->foreignIdFor(User::class)->after('plan_id')->constrained();
+
+            $table->foreign('plan_id')->references('id')->on('plans');
+
+            $table->unique(['plan_id', 'user_id'], 'PSI');
+        });
+    }
+};
