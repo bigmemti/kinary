39ffe26c8b958051cf0e5bdc3b06003d11b344b3
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-13 19:15:02 +0330

    add takeInRandomOrder and pluckInRandomOrder in model base

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model as ModelEloquent;
 
 class Model extends ModelEloquent{
+    public static function takeInRandomOrder(int|null $take = null, $seed = ''){
+        return self::inRandomOrder($seed)->take($take);
+    }
+
     public static function getInRandomOrder(int|null $take = null, $columns = ['*'], $seed = ''){
-        return self::inRandomOrder($seed)->take($take)->get($columns);
+        return self::takeInRandomOrder($take, $seed)->get($columns);
+    }
+ 
+    public static function pluckInRandomOrder(int|null $take = null, $columns = ['*'], $seed = ''){
+        return self::takeInRandomOrder($take, $seed)->pluck($columns);
     }
 }
