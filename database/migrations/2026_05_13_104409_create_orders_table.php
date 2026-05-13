2af6e58f99107bf87740b3cd4239e42ce04f69fd
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-13 19:29:35 +0330

    fix order status column

--- database/migrations/2026_05_13_104409_create_orders_table.php
+++ database/migrations/2026_05_13_104409_create_orders_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Wallet::class)->constrained();
-            $table->enum('status', ['pending', 'success', 'failed'])->default('pending');
+            $table->enum('status', ['pending', 'paid'])->default('pending');
             $table->timestamps();
         });
     }
