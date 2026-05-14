f0560785f5952596067677ec49ffa869c00e5471
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-15 00:01:36 +0330

    add responsive data list

---
+++ resources/js/components/responsive-data-list.tsx
@@ -0,0 +1,74 @@
+import { useIsLarge } from "@/hooks/use-breakpoint";
+
+type Column<T> = {
+    header: React.ReactNode
+    cell: (item: T) => React.ReactNode
+    card?: (item: T) => React.ReactNode
+}
+
+type Props<T> = {
+    data: T[]
+    columns: Column<T>[]
+}
+
+export default function ResponsiveDataList<T>({ data, columns }: Props<T>) {
+    const isLarge = useIsLarge()
+
+    return (
+        <>
+            {(isLarge) 
+                ?  <DataTable data={data} columns={columns} /> 
+                :  <DataCard data={data} columns={columns} />
+            }
+        </>
+    );
+}
+// TODO: refactor
+function DataCard<T>({ data, columns }: Props<T>) {
+    return(
+        <ul className="grid lg:grid-cols-2 gap-4">
+            {data.map((item, i) => (
+                <li key={i}>
+                    {columns.map((col, j) => (
+                        <div key={j}>
+                            {col.header} :  {col.card? col.card(item): col.cell(item)}
+                        </div>
+                    ))}
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+function DataTable<T>({ data, columns }: Props<T>) {
+    return(
+        <table className="border-separate border-spacing-y-2">
+            <thead>
+                <tr>
+                    {columns.map((col, i) => (
+                        <th key={i}>{col.header}</th>
+                    ))}
+                </tr>
+            </thead>
+
+            <tbody>
+                {data.map((item, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {columns.map((col, colIndex) => (
+                            <td key={colIndex} className="text-center">
+                                {col.cell(item)}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+            <tfoot>
+                <tr>
+                    {columns.map((col, i) => (
+                        <th key={i}>{col.header}</th>
+                    ))}
+                </tr>
+            </tfoot>
+        </table>
+    );
+}
