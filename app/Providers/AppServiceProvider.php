da36afc06ecda1230c3df62ec4d06cfcafa7d655
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-14 10:00:05 +0330

    add softDeletableResource marco to route facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Observers\UserObserver;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +23,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         User::observe(UserObserver::class);
+
+        Route::macro('softDeletableResource', function($name, $controller, $options = []){
+            Route::resource($name, $controller, $options);
+            Route::post("$name/{{$name}}/restore", [$controller, 'restore']);
+            Route::delete("$name/{{$name}}/fore-delete", [$controller, 'foreDelete']);
+        });
     }
 }
