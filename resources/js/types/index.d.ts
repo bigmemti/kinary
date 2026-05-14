6f506a8624e8950f68ed454fdaacce9e1e418dd9
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-14 22:47:29 +0330

    fix student and teacher types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -36,6 +36,8 @@ export interface User {
     email: string;
     avatar?: string;
     wallet?: Wallet
+    student?: Student
+    teacher?: Teacher
     email_verified_at: string | null;
     two_factor_enabled?: boolean;
     created_at: string;
@@ -51,9 +53,25 @@ export interface Wallet{
     created_at: DateTime;
     updated_at: DateTime;
 }
+export interface Student{
+    id: number;
+    user_id: number;
+    user?: User;
+    created_at: DateTime;
+    updated_at: DateTime;
+}
+export interface Teacher{
+    id: number;
+    user_id: number;
+    user?: User;
+    created_at: DateTime;
+    updated_at: DateTime;
+}
 
 export interface Course{
     id: number;
+    teacher_id: number;
+    teacher?: Teacher
     title: string;
     slug: string;
     description: string;
