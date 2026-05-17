1d93f63caca28238283f810ebf87134524d07430
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-17 20:21:17 +0330

    add enrollment fillable vars

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -4,6 +4,16 @@ namespace App\Models;
 
 class Enrollment extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'student_id',
+        'plan_id',
+    ];
+
     public function student(){
         return $this->belongsTo(Student::class);
     }
