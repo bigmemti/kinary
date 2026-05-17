d5239d0775b8c0a0adc685c93198ef19c7d7354b
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-17 23:20:16 +0330

    add pagination component

---
+++ resources/js/components/pagination.tsx
@@ -0,0 +1,41 @@
+import { Pagination as BasePagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "./ui/pagination"
+
+export default function Pagination({ meta }: any) {
+
+    const pages = []
+
+    for (let i = 1; i <= meta.last_page; i++) {
+        pages.push(i)
+    }
+
+    return (
+        <BasePagination>
+            <PaginationContent>
+
+                <PaginationItem>
+                    <PaginationPrevious
+                        href={meta.prev_page_url}
+                    />
+                </PaginationItem>
+
+                {pages.map(page => (
+                    <PaginationItem key={page}>
+                        <PaginationLink
+                            isActive={page === meta.current_page}
+                            href={`?page=${page}`}
+                        >
+                            {page}
+                        </PaginationLink>
+                    </PaginationItem>
+                ))}
+
+                <PaginationItem>
+                    <PaginationNext
+                        href={meta.next_page_url}
+                    />
+                </PaginationItem>
+
+            </PaginationContent>
+        </BasePagination>
+    )
+}
