075197a29b8fb1fbf9e1e8658fbea0a0abe0adde
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-05-26 03:18:30 +0330

    delete all uses from admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,63 +1,44 @@
 <?php
 
-use App\Http\Controllers\Admin\ContentController;
-use App\Http\Controllers\Admin\CourseController;
-use App\Http\Controllers\Admin\CoursePlanController;
-use App\Http\Controllers\Admin\CourseSectionController;
-use App\Http\Controllers\Admin\EnrollmentController;
-use App\Http\Controllers\Admin\LessonContentController;
-use App\Http\Controllers\Admin\LessonController;
-use App\Http\Controllers\Admin\OrderController;
-use App\Http\Controllers\Admin\OrderPlanController;
-use App\Http\Controllers\Admin\OrderTransactionController;
-use App\Http\Controllers\Admin\PlanController;
-use App\Http\Controllers\Admin\PlanEnrollmentController;
-use App\Http\Controllers\Admin\PlanOrderController;
-use App\Http\Controllers\Admin\SectionController;
-use App\Http\Controllers\Admin\SectionLessonController;
-use App\Http\Controllers\Admin\StudentController;
-use App\Http\Controllers\Admin\StudentEnrollmentController;
-use App\Http\Controllers\Admin\TeacherController;
-use App\Http\Controllers\Admin\TeacherCourseController;
-use App\Http\Controllers\Admin\TransactionController;
-use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\Admin\WalletController;
-use App\Http\Controllers\Admin\WalletOrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
-    Route::resource('user', UserController::class);
+Route::middleware(['auth', 'verified'])
+    ->namespace('App\Http\Controllers\Admin')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::resource('user', 'UserController');
 
-    Route::resource('student', StudentController::class);
-    Route::resource('student.enrollment', StudentEnrollmentController::class)->only(['index', 'create', 'store'])->shallow();
+        Route::resource('student', 'StudentController');
+        Route::resource('student.enrollment', 'StudentEnrollmentController')->only(['index', 'create', 'store'])->shallow();
 
-    Route::resource('wallet', WalletController::class);
-    Route::resource('wallet.order', WalletOrderController::class)->only(['index', 'create', 'store'])->shallow();
+        Route::resource('wallet', 'WalletController');
+        Route::resource('wallet.order', 'WalletOrderController')->only(['index', 'create', 'store'])->shallow();
 
-    Route::resource('teacher', TeacherController::class);
-    Route::resource('teacher.course', TeacherCourseController::class)->only(['index', 'create', 'store'])->shallow();
+        Route::resource('teacher', 'TeacherController');
+        Route::resource('teacher.course', 'TeacherCourseController')->only(['index', 'create', 'store'])->shallow();
 
-    Route::resource('course', CourseController::class);
-    Route::resource('course.plan', CoursePlanController::class)->only(['index', 'create', 'store'])->shallow();
-    Route::resource('course.section', CourseSectionController::class)->only(['index', 'create', 'store'])->shallow();
-    
-    Route::resource('plan', PlanController::class);
-    Route::resource('plan.order', PlanOrderController::class)->only(['index'])->shallow();
-    Route::resource('plan.enrollment', PlanEnrollmentController::class)->only(['index'])->shallow();
-    
-    Route::resource('section', SectionController::class);
-    Route::resource('section.lesson', SectionLessonController::class)->only(['index', 'create', 'store'])->shallow();
-    
-    Route::resource('lesson', LessonController::class);
-    Route::resource('lesson.content', LessonContentController::class)->only(['index', 'create', 'store'])->shallow();
-    
-    Route::resource('content', ContentController::class);
-    
-    Route::resource('enrollment', EnrollmentController::class);
-    
-    Route::resource('order', OrderController::class);
-    Route::resource('order.plan', OrderPlanController::class)->only(['index'])->shallow();
-    Route::resource('order.transaction', OrderTransactionController::class)->only(['index', 'create', 'store'])->shallow();
+        Route::resource('course', 'CourseController');
+        Route::resource('course.plan', 'CoursePlanController')->only(['index', 'create', 'store'])->shallow();
+        Route::resource('course.section', 'CourseSectionController')->only(['index', 'create', 'store'])->shallow();
+        
+        Route::resource('plan', 'PlanController');
+        Route::resource('plan.order', 'PlanOrderController')->only(['index'])->shallow();
+        Route::resource('plan.enrollment', 'PlanEnrollmentController')->only(['index'])->shallow();
+        
+        Route::resource('section', 'SectionController');
+        Route::resource('section.lesson', 'SectionLessonController')->only(['index', 'create', 'store'])->shallow();
+        
+        Route::resource('lesson', 'LessonController');
+        Route::resource('lesson.content', 'LessonContentController')->only(['index', 'create', 'store'])->shallow();
+        
+        Route::resource('content', 'ContentController');
+        
+        Route::resource('enrollment', 'EnrollmentController');
+        
+        Route::resource('order', 'OrderController');
+        Route::resource('order.plan', 'OrderPlanController')->only(['index'])->shallow();
+        Route::resource('order.transaction', 'OrderTransactionController')->only(['index', 'create', 'store'])->shallow();
 
-    Route::resource('transaction', TransactionController::class);
+        Route::resource('transaction', 'TransactionController');
 });
