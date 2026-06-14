2d5ff655d9003303c714041ff2a11f7e2dc8c446
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-06-14 14:11:37 +0330

    some style in front end

--- resources/views/course-show.blade.php
+++ resources/views/course-show.blade.php
@@ -1,15 +1,19 @@
 <x-layouts.web>
-    <div class="dark:text-white">{{ $course->title }}</div>
-    <div class="dark:text-white">{{ $course->teacher->user->name }}:</div>
-    @forelse ($course->plans as $plan)
-        <div class="dark:text-white">
-            <div>{{ $plan->name }}</div>
-            <div>{{ $plan->price }}</div>
-            <div>
-                <a href="{{ route('plan.buy', ['plan' => $plan]) }}">buy</a>
-            </div>
+    <div class="flex flex-col gap-2 items-end">
+        <div class="dark:text-white">{{ $course->title }}</div>
+        <div class="dark:text-white">{{ $course->teacher->user->name }}:</div>
+        <div class="w-full self-stretch flex justify-around py-4">
+            @forelse ($course->plans as $plan)
+                <div class="dark:text-white border bg-teal-800 p-10 rounded-xl flex flex-col items-end gap-4 py-16">
+                    <div>{{ $plan->name }}</div>
+                    <div>{{ $plan->price }}</div>
+                    <div>
+                        <a class="border px-8 p-2" href="{{ route('plan.buy', ['plan' => $plan]) }}">buy</a>
+                    </div>
+                </div>
+            @empty
+                
+            @endforelse
         </div>
-    @empty
-        
-    @endforelse
+    </div>
 </x-layouts.web>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,15 @@
 <x-layouts.web>
-    @forelse ($courses as $course)
-        <div class="dark:text-white">
-            <div>{{ $course->title }}</div>
-            <div>
-                <a href="{{ route('course.show', ['course' => $course]) }}">show</a>
+    <div class="flex gap-2 ">
+        @forelse ($courses as $course)
+            <div class="dark:text-white border p-4 space-y-2">
+                <img src="{{ $course->thumbnail }}" class="w-80 aspect-video" alt="">
+                <div class="text-end">{{ $course->title }}</div>
+                <div class="text-center">
+                    <a class="border p-2 px-8 bg-teal-800" href="{{ route('course.show', ['course' => $course]) }}">show</a>
+                </div>
             </div>
-        </div>
-    @empty
-        
-    @endforelse
+        @empty
+            
+        @endforelse
+    </div>
 </x-layouts.web>
