216d5ba36102695fc4a96ce17aa707b432c557c6
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-07-08 12:39:56 +0330

    add course teacher table

---
+++ database/migrations/2026_07_08_090303_create_course_teacher_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Course;
+use App\Models\Teacher;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_teacher', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Course::class)->constrained();
+            $table->foreignIdFor(Teacher::class)->constrained();
+            $table->enum('role', ['owner', 'mate', 'editor']);
+            $table->timestamps();
+
+            $table->unique(['course_id', 'teacher_id'], 'CTI');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_teacher');
+    }
+};
