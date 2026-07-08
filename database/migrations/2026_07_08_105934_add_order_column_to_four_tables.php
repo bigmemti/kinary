d94634d1c667129faa0b819d61314ed9be150a33
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-07-08 14:35:27 +0330

    add order to plans sections lessons contents

---
+++ database/migrations/2026_07_08_105934_add_order_column_to_four_tables.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('plans', function (Blueprint $table) {
+            $table->smallInteger('order')->after('price');
+        });
+
+        Schema::table('sections', function (Blueprint $table) {
+            $table->smallInteger('order')->after('name');
+        });
+
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->smallInteger('order')->after('name');
+        });
+
+        Schema::table('contents', function (Blueprint $table) {
+            $table->smallInteger('order')->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('plans', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+
+        Schema::table('sections', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+
+        Schema::table('contents', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+    }
+};
