29e6d80a34a31b68d9f128c9f176cc7efd3cb22f
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-07-08 14:42:20 +0330

    make content polymorph

---
+++ database/migrations/2026_07_08_110641_make_ploymorph_contents_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contents', function (Blueprint $table) {
+            $table->morphs('contentable', after: 'lesson_id');
+            $table->renameColumn('body', 'description');
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contents', function (Blueprint $table) {
+            $table->dropMorphs('contentable');
+            $table->renameColumn('description', 'body');
+        });
+    }
+};
