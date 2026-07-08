b5f31b07cdbcbf4f600b606d2eb381fa0e2558ee
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-07-08 14:20:49 +0330

    add progress model and table

---
+++ database/migrations/2026_07_08_092353_create_progress_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Content;
+use App\Models\Enrollment;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('progress', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Enrollment::class)->constrained();
+            $table->foreignIdFor(Content::class)->constrained();
+            $table->enum('status', ['unread', 'read', 'cloned'])->default('unread');
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('progress');
+    }
+};
