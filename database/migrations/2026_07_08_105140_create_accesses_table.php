d41fb6489608cb357f07f07f90ebe54218b938c1
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-07-08 14:23:00 +0330

    add access model and table

---
+++ database/migrations/2026_07_08_105140_create_accesses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Content;
+use App\Models\Plan;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accesses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Plan::class)->constrained();
+            $table->foreignIdFor(Content::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accesses');
+    }
+};
