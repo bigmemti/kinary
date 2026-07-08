ae1ea4d025c96fc730db0d5d466010f608fc3949
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-07-08 14:27:26 +0330

    add requirement model and table

---
+++ database/migrations/2026_07_08_105339_create_requirements_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Access;
+use App\Models\Space;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requirements', function (Blueprint $table) {
+            $table->foreignIdFor(Access::class)->constrained();
+            $table->foreignIdFor(Space::class)->constrained();
+            $table->smallInteger('score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requirements');
+    }
+};
