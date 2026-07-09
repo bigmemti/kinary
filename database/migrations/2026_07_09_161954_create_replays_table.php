2dd6de336ea59514069292f4b678dd3d3970c989
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-07-09 19:55:02 +0330

    add Collaboration module

---
+++ database/migrations/2026_07_09_161954_create_replays_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Discussion;
+use App\Models\Replay;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('replays', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Discussion::class)->constrained();
+            $table->foreignIdFor(Replay::class)->constrained();
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('replays');
+    }
+};
