acafbb73d2d321ae6678716e2b82646e99a9502d
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-07-09 19:42:19 +0330

    add quiz id to spaces

---
+++ database/migrations/2026_07_08_201813_add_quiz_id_to_spaces_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Quiz;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('spaces', function (Blueprint $table) {
+            $table->foreignIdFor(Quiz::class)->after('course_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('spaces', function (Blueprint $table) {
+            $table->dropConstrainedForeignIdFor(Quiz::class);
+        });
+    }
+};
