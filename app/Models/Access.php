ee1f88bb8c757fb8cb4e7b66058d1187fabb02e8
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2026-07-12 03:47:14 +0330

    add fillable and relations to access model

--- app/Models/Access.php
+++ app/Models/Access.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Access extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\LessonFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'plan_id',
+        'content_id',
+    ];
+
+    public function plan(){
+        return $this->belongsTo(Plan::class);
+    }
+
+    public function content(){
+        return $this->belongsTo(Content::class);
+    }
+
+    public function requirements(){
+        return $this->hasMany(Requirement::class);
+    }
 }
